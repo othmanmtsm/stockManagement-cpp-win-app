35b44e422e3eff029bfd2be3bc2cb4dbb35802d4
othmanmtsm <motassimothman@gmail.com>
2020-05-20 00:02:17 +0000

    Add project files.

---
+++ stockManagement/mainForm.cpp
@@ -0,0 +1,12 @@
+#include "mainForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    stockManagement::mainForm form;
+    Application::Run(% form);
+}
